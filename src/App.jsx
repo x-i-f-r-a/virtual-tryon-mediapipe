0df6c6d30ead6bd1f7c08c4be1e2829c6ce716e0
Gokul-Krishnan24 <gokul.krishnan@luitvalley.in>
2025-05-16 17:30:29 +0530

    update ecommerce site with virtual try on

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Bestsellers from "./pages/Bestsellers";
+import Header from "./Components/Header/Header";
+import Bestsellers from "./Pages/Bestsellers";
 import ProductDetails from "./Pages/ProductDetails";
 import ProductList from "./Components/productList/productList";
 import Footer from "./Components/Footer/footer";
